54a0d4a49ce4494d30342048bb4ac7de05598f12
phuongnvp <phuongnvp1011@gmail.com>
2023-11-01 16:09:06 +0700

    Update file

--- streamlit.py
+++ streamlit.py
@@ -33,6 +33,7 @@ if smiles_input:
     
     data_entries.extend(entries)
 
+st.write("Các công thức smiles bạn đã nhập:")
 for smiles in data_entries:
     st.write(smiles)
     
@@ -59,7 +60,7 @@ if st.button('Result') and data_entries:
                                                'mol',
                                               'sentence', 'Smiles'], axis=1)), axis=1)
 # Load pretrained model
-             model = joblib.load('Desktop/model_fyn.pkl')
+             model = joblib.load('Desktop/model.pkl')
               
              y_prediction = model.predict(X.iloc[[i]].values)
              probs1 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 1] * 100, 2)
