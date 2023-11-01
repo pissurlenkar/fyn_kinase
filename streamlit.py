7d82a3e54f53d23119c5f59296017c586ad2d976
phuongnvp <phuongnvp1011@gmail.com>
2023-11-01 16:49:16 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -48,7 +48,7 @@ if selected_mode == "Single Mode":
         X1 = np.array([x.vec for x in df1['mol2vec']])  
         X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 'mol', 'sentence', 'Smiles'], axis=1)), axis=1)
     # Load pretrained model
-        model = joblib.load('model.pkl')
+        model = joblib.load('model_fyn.pkl')
         y_prediction = model.predict(X.values)
         probs1 = np.round(model.predict_proba(X.values)[:, 1] * 100, 2)
         probs0 = np.round(model.predict_proba(X.values)[:, 0] * 100, 2)
@@ -58,62 +58,57 @@ if selected_mode == "Single Mode":
             result = ('Your compound is inactive with probality of', f'{probs0[0]}%')
         st.success(result)
 else:
-    st.write("You are in Batch Mode.")
+    data_entries = []
+    if smiles_input:
+        entries = smiles_input.split('\n')
+        
+        data_entries.extend(entries)
 
+    if st.button('Result') and data_entries: 
+        #longle = df.loc[(df['Smiles'] == smiles_input)]
+        df1 = pd.DataFrame({'Smiles':data_entries})
+        for i in range(len(df1)):
+                 df1['mol'] = df1['Smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
+                 df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
+        #
+                 from mol2vec.features import mol2alt_sentence, mol2sentence, MolSentence, DfVec, sentences2vec
+                 from gensim.models import word2vec
+                 w2vec_model = word2vec.Word2Vec.load('model_300dim.pkl')
+                 df1['sentence'] = df1.apply(lambda x: MolSentence(mol2alt_sentence(x['mol'], 1)), axis=1)
+                 df1['mol2vec'] = [DfVec(x) for x in sentences2vec(df1['sentence'], w2vec_model, unseen='UNK')]
+        
+        # Create dataframe 
+                 X1 = np.array([x.vec for x in df1['mol2vec']])  
+                 X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 
+                                                   'mol',
+                                                  'sentence', 'Smiles'], axis=1)), axis=1)
+    # Load pretrained model
+                 model = joblib.load('model.pkl')
+                  
+                 y_prediction = model.predict(X.iloc[[i]].values)
+                 probs1 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 1] * 100, 2)
+                 probs0 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 0] * 100, 2)
+                 #if y_prediction[0] == 1: 
+                  #   Predict_Result1 = f'Kết quả dự đoán cho hàng {i + 1}: Incompatible. Xác suất: {probs1[0]}%'
+                 #else: 
+                  #   Predict_Result1 = f'Kết quả dự đoán cho hàng {i + 1}: Compatible. Xác suất: {probs0[0]}%'
+                 if y_prediction[0] == 1:
+                     result = ('Active', f'{probs1[0]}%')
+                 else:
+                     result = ('Inactive', f'{probs0[0]}%')
+                 results1.append(result)
+    longle = results1
+    for i, (prediction, probability) in enumerate(longle):
+        st.success(f'Kết quả dự đoán cho hàng {i + 1}: {prediction}. Xác suất: {probability}')
+    #Hiển thị dataframe
+    df3 = pd.DataFrame(longle)
+    st.dataframe(df3)
+    # Xuất file
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')  
+    csv = convert_df(pd.DataFrame(longle, columns=['Prediction', 'Probability']))
 
-data_entries = []
-if smiles_input:
-    entries = smiles_input.split('\n')
-    
-    data_entries.extend(entries)
-
-if st.button('Result') and data_entries: 
-    #longle = df.loc[(df['Smiles'] == smiles_input)]
-    df1 = pd.DataFrame({'Smiles':data_entries})
-    for i in range(len(df1)):
-             df1['mol'] = df1['Smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
-             df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
-    #
-             from mol2vec.features import mol2alt_sentence, mol2sentence, MolSentence, DfVec, sentences2vec
-             from gensim.models import word2vec
-             w2vec_model = word2vec.Word2Vec.load('model_300dim.pkl')
-             df1['sentence'] = df1.apply(lambda x: MolSentence(mol2alt_sentence(x['mol'], 1)), axis=1)
-             df1['mol2vec'] = [DfVec(x) for x in sentences2vec(df1['sentence'], w2vec_model, unseen='UNK')]
-    
-    # Create dataframe 
-             X1 = np.array([x.vec for x in df1['mol2vec']])  
-             X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 
-                                               'mol',
-                                              'sentence', 'Smiles'], axis=1)), axis=1)
-# Load pretrained model
-             model = joblib.load('model.pkl')
-              
-             y_prediction = model.predict(X.iloc[[i]].values)
-             probs1 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 1] * 100, 2)
-             probs0 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 0] * 100, 2)
-             #if y_prediction[0] == 1: 
-              #   Predict_Result1 = f'Kết quả dự đoán cho hàng {i + 1}: Incompatible. Xác suất: {probs1[0]}%'
-             #else: 
-              #   Predict_Result1 = f'Kết quả dự đoán cho hàng {i + 1}: Compatible. Xác suất: {probs0[0]}%'
-             if y_prediction[0] == 1:
-                 result = ('Active', f'{probs1[0]}%')
-             else:
-                 result = ('Inactive', f'{probs0[0]}%')
-             results1.append(result)
-longle = results1
-for i, (prediction, probability) in enumerate(longle):
-    st.success(f'Kết quả dự đoán cho hàng {i + 1}: {prediction}. Xác suất: {probability}')
-#Hiển thị dataframe
-df3 = pd.DataFrame(longle)
-st.dataframe(df3)
-# Xuất file
-def convert_df(df):
-    return df.to_csv().encode('utf-8')  
-csv = convert_df(pd.DataFrame(longle, columns=['Prediction', 'Probability']))
-
-st.download_button(label="Download results of prediction as CSV",
-                    data=csv, file_name='Results.csv',
-                    mime='text/csv',
-                )
-
-
+    st.download_button(label="Download results of prediction as CSV",
+                        data=csv, file_name='Results.csv',
+                        mime='text/csv',
+                    )
