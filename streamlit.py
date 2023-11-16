2a951df39736138d5970718c87dd77b58904d011
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-11-16 17:55:33 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd               
 import matplotlib.pyplot as plt   
 import seaborn as sns
+import rdkit
 from rdkit.Chem import Draw, Descriptors, rdqueries
 from rdkit import RDLogger  
 RDLogger.DisableLog('rdApp.*')  
