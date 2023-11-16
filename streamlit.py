d9ffcbcf505c546afa62aa59da3c0aca8716568d
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-11-16 14:09:29 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,8 +24,9 @@ import io
 import subprocess
 from xgboost import XGBClassifier
 from PIL import Image
-import meeko
 from vina import Vina
+import meeko
+
 
 # Calculate Application Domain
 df = pd.read_csv('Fyn_kinase.csv')
