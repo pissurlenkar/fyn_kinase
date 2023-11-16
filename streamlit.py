b37cf31fde78218b4bd2072ee5fe9b2cfd662980
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-11-16 13:39:06 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -55,108 +55,111 @@ def convert_df(df):
 #st.title('FYN KINASE SCREENING')
 st.markdown("<h1 style='text-align: center;'>FYN KINASE SCREENING</h1>", unsafe_allow_html=True)
 
-st.title("Mode Selector")
-selected_mode = st.sidebar.selectbox("Select Mode", ["QSAR", "Docking"])
+st.title("Mode of Screening")
+selected_mode = st.sidebar.selectbox("Select Mode", ["QSAR", "Molecular docking"])
 
-# Create a radio button to choose the mode
-selected_mode = st.selectbox("Select Mode of Screening", ["Single Mode", "Batch Mode"])
+if selected_mode == "QSAR":
+    # Create a radio button to choose the mode
+    selected_mode = st.selectbox("Select Mode of Screening", ["Single Mode", "Batch Mode"])
 
-# Depending on the user's choice, display different content
-if selected_mode == "Single Mode":
-    smiles_input = st.text_input("Enter your SMILES string!")
-    if st.button('Result'):
-        df1 = pd.DataFrame({'Smiles': smiles_input},index=[0])
-        df1['mol'] = df1['Smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
-        df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
-        # Calculate mol2vec descriptors
-        from mol2vec.features import mol2alt_sentence, mol2sentence, MolSentence, DfVec, sentences2vec
-        from gensim.models import word2vec
-        w2vec_model = word2vec.Word2Vec.load('model_300dim.pkl')
-        df1['sentence'] = df1.apply(lambda x: MolSentence(mol2alt_sentence(x['mol'], 1)), axis=1)
-        df1['mol2vec'] = [DfVec(x) for x in sentences2vec(df1['sentence'], w2vec_model, unseen='UNK')]
-        # Create dataframe 
-        X1 = np.array([x.vec for x in df1['mol2vec']])  
-        X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 'mol', 'sentence', 'Smiles'], axis=1)), axis=1)
-        #Application of Domain
-        distances_screen,_ = knn_model.kneighbors(X)
-        Di = np.mean(distances_screen)
-        if Di > threshold:
-            result = 'Your compound is out of our application domain'
-        else:
-            # Load pretrained model
-            model = joblib.load('model_fyn.pkl')
-            y_prediction = model.predict(X.values)
-            probs1 = np.round(model.predict_proba(X.values)[:, 1] * 100, 2)
-            probs0 = np.round(model.predict_proba(X.values)[:, 0] * 100, 2)
-            if y_prediction[0] == 1:
-                result = f'Your compound is active with probality of {probs1[0]}%'
-            else:
-                result = f'Your compound is inactive with probality of {probs0[0]}%'
-        st.success(result)
-else:
-    smiles_input = st.text_area('Enter your SMILES strings! (One SMILES per line)')
-    data_entries = []
-    if smiles_input:
-        entries = smiles_input.split('\n')        
-        data_entries.extend(entries)
-    if st.button('Result'):
-        df1 = pd.DataFrame({'Smiles': data_entries})
-        structure_images = []
-        img_bytes_list = []
-        for i in range(len(df1)):
+    # Depending on the user's choice, display different content
+    if selected_mode == "Single Mode":
+        smiles_input = st.text_input("Enter your SMILES string!")
+        if st.button('Result'):
+            df1 = pd.DataFrame({'Smiles': smiles_input},index=[0])
             df1['mol'] = df1['Smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
             df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
-            
+            # Calculate mol2vec descriptors
             from mol2vec.features import mol2alt_sentence, mol2sentence, MolSentence, DfVec, sentences2vec
             from gensim.models import word2vec
             w2vec_model = word2vec.Word2Vec.load('model_300dim.pkl')
             df1['sentence'] = df1.apply(lambda x: MolSentence(mol2alt_sentence(x['mol'], 1)), axis=1)
             df1['mol2vec'] = [DfVec(x) for x in sentences2vec(df1['sentence'], w2vec_model, unseen='UNK')]
-        
-        # Creat dataframe 
+            # Create dataframe 
             X1 = np.array([x.vec for x in df1['mol2vec']])  
-            X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 
-                                                   'mol',
-                                                  'sentence', 'Smiles'], axis=1)), axis=1)
-            
-            distances_screen,_ = knn_model.kneighbors(X.iloc[[i]])
+            X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 'mol', 'sentence', 'Smiles'], axis=1)), axis=1)
+            #Application of Domain
+            distances_screen,_ = knn_model.kneighbors(X)
             Di = np.mean(distances_screen)
-            
             if Di > threshold:
-                act = 'Not Determine'
-                probs = 'Not Determine'
-                note = 'Outside of Application Domain'
+                result = 'Your compound is out of our application domain'
             else:
                 # Load pretrained model
-                model = joblib.load('model_fyn.pkl')                 
-                y_prediction = model.predict(X.iloc[[i]].values)
-                probs1 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 1] * 100, 2)
-                probs0 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 0] * 100, 2)                
+                model = joblib.load('model_fyn.pkl')
+                y_prediction = model.predict(X.values)
+                probs1 = np.round(model.predict_proba(X.values)[:, 1] * 100, 2)
+                probs0 = np.round(model.predict_proba(X.values)[:, 0] * 100, 2)
                 if y_prediction[0] == 1:
-                    act = 'Active'
-                    probs = probs1[0]
-                    note = ''
+                    result = f'Your compound is active with probality of {probs1[0]}%'
                 else:
-                    act = 'Inactive'
-                    probs = probs0[0]
-                    note = ''
-
-            probability.append(probs)
-            activity.append(act)
-            AD.append(note)
+                    result = f'Your compound is inactive with probality of {probs0[0]}%'
+            st.success(result)
+    else:
+        smiles_input = st.text_area('Enter your SMILES strings! (One SMILES per line)')
+        data_entries = []
+        if smiles_input:
+            entries = smiles_input.split('\n')        
+            data_entries.extend(entries)
+        if st.button('Result'):
+            df1 = pd.DataFrame({'Smiles': data_entries})
+            structure_images = []
+            img_bytes_list = []
+            for i in range(len(df1)):
+                df1['mol'] = df1['Smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
+                df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
+                
+                from mol2vec.features import mol2alt_sentence, mol2sentence, MolSentence, DfVec, sentences2vec
+                from gensim.models import word2vec
+                w2vec_model = word2vec.Word2Vec.load('model_300dim.pkl')
+                df1['sentence'] = df1.apply(lambda x: MolSentence(mol2alt_sentence(x['mol'], 1)), axis=1)
+                df1['mol2vec'] = [DfVec(x) for x in sentences2vec(df1['sentence'], w2vec_model, unseen='UNK')]
             
+            # Creat dataframe 
+                X1 = np.array([x.vec for x in df1['mol2vec']])  
+                X = pd.concat((pd.DataFrame(X1), df1.drop(['mol2vec', 
+                                                       'mol',
+                                                      'sentence', 'Smiles'], axis=1)), axis=1)
+                
+                distances_screen,_ = knn_model.kneighbors(X.iloc[[i]])
+                Di = np.mean(distances_screen)
+                
+                if Di > threshold:
+                    act = 'Not Determine'
+                    probs = 'Not Determine'
+                    note = 'Outside of Application Domain'
+                else:
+                    # Load pretrained model
+                    model = joblib.load('model_fyn.pkl')                 
+                    y_prediction = model.predict(X.iloc[[i]].values)
+                    probs1 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 1] * 100, 2)
+                    probs0 = np.round(model.predict_proba(X.iloc[[i]].values)[:, 0] * 100, 2)                
+                    if y_prediction[0] == 1:
+                        act = 'Active'
+                        probs = probs1[0]
+                        note = ''
+                    else:
+                        act = 'Inactive'
+                        probs = probs0[0]
+                        note = ''
+    
+                probability.append(probs)
+                activity.append(act)
+                AD.append(note)
+                
+    
+            df3 = pd.DataFrame({
+                'Compound': data_entries,
+                'Predicted Activity': activity,
+                'Probability (%)': probability,
+                'Note': AD
+                })
+            st.dataframe(df3)
+            
+            st.download_button(
+                label="Download results as CSV file",
+                data=df3.to_csv(index=False),
+                file_name='Results.csv',
+                mime='text/csv'
+                )
 
-        df3 = pd.DataFrame({
-            'Compound': data_entries,
-            'Predicted Activity': activity,
-            'Probability (%)': probability,
-            'Note': AD
-            })
-        st.dataframe(df3)
-        
-        st.download_button(
-            label="Download results as CSV file",
-            data=df3.to_csv(index=False),
-            file_name='Results.csv',
-            mime='text/csv'
-            )
+if selected_mode == "Molecular docking":
