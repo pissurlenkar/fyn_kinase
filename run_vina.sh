23d0ebc8a254f6d071327e1bec6d6a06b6be932d
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-11-17 23:18:21 +0700

    Update run_vina.sh

--- run_vina.sh
+++ run_vina.sh
@@ -2,7 +2,7 @@
 
 for f in ligand_*.pdbqt; do
     b=`basename $f .pdbqt`
-    echo Processing ligand $b
+    echo Processing $b
     mkdir -p $b
     vina --config config.txt --ligand $f --out ${b}/out.pdbqt --log ${b}/log.txt
 done
